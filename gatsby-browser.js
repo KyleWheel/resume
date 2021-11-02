3658c5b68aec4d5b98e4eb0297e3e55bb73160c1
Kyle Wheeler <14957527+KyleWheel@users.noreply.github.com>
2021-11-02 14:52:13 -0700

    Add font weight and some styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@
 import "@fontsource/ibm-plex-sans" // Defaults to weight 400 with all styles included.
 import "@fontsource/ibm-plex-sans/200.css" 
 import "@fontsource/ibm-plex-sans/700.css" 
-import "@fontsource/ibm-plex-mono" // Defaults to weight 400 with all styles included.
 
+import "@fontsource/ibm-plex-mono" // Defaults to weight 400 with all styles included.
+import "@fontsource/ibm-plex-mono/700.css" 
 
